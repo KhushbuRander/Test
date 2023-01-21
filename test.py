bd33e539bc4def81d76faf906e216312d667c621
KhushbuRander <123160999+KhushbuRander@users.noreply.github.com>
2023-01-21 11:17:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,39 +10,6 @@ from selenium.common.exceptions import NoSuchElementException
 # without any changes to the test files!
 #driver = webdriver.Chrome()
 
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-userName = os.environ['BROWSERSTACK_USERNAME']
-accessKey = os.environ['BROWSERSTACK_ACCESS_KEY']
-localIdentifier = os.environ['BROWSERSTACK_LOCAL_IDENTIFIER']
-buildName = os.environ['BROWSERSTACK_BUILD_NAME']
-projectName = os.environ['BROWSERSTACK_PROJECT_NAME']
-
-desired_cap = {
-    'bstack:options' : {
-        "os" : "Windows",
-        "osVersion" : "10",
-        "buildName" : "BStack Build Name: " + buildName,
-        "projectName" : "BStack Project Name: " + projectName,
-        "localIdentifier": localIdentifier,
-        "seleniumVersion" : "4.0.0",
-        "userName": username,
-        "accessKey": accessKey
-        },
-    "browserName" : "Chrome",
-    "browserVersion" : "100.0",
-    }
-options = ChromeOptions()
-options.set_capability('bstack:options', bstack_options)
-driver = webdriver.Remote(
-    command_executor="https://hub.browserstack.com/wd/hub",
-    options=options)
-driver.get("http://localhost:8099") # HTTP Server should be running on 8099 port of GitHub runner
-print(driver.title)
-driver.quit()
-
-
 try:
     driver.get('https://bstackdemo.com/')
     WebDriverWait(driver, 10).until(EC.title_contains('StackDemo'))
