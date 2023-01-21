82a46bd8717b846f42e13d2034e91854cf6698b0
KhushbuRander <123160999+KhushbuRander@users.noreply.github.com>
2023-01-21 16:04:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -40,40 +40,4 @@ driver = webdriver.Remote(
     options=options)
 driver.get("http://localhost:8099") # HTTP Server should be running on 8099 port of GitHub runner
 print(driver.title)
-#driver.quit()
-
-try:
-    driver.get('https://bstackdemo.com/')
-    WebDriverWait(driver, 10).until(EC.title_contains('StackDemo'))
-    # Get text of an product - iPhone 12
-    item_on_page = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, '//*[@id="1"]/p'))).text
-    # Click the 'Add to cart' button if it is visible
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-        (By.XPATH, '//*[@id="1"]/div[4]'))).click()
-    # Check if the Cart pane is visible
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-        (By.CLASS_NAME, 'float-cart__content')))
-    # Get text of product in cart
-    item_in_cart = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-        (By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'))).text
-    # Verify whether the product (iPhone 12) is added to cart
-    if item_on_page == item_in_cart:
-        # Set the status of test as 'passed' if item is added to cart
-        driver.execute_script(
-            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "iPhone 12 has been successfully added to the cart!"}}')
-    else:
-        # Set the status of test as 'failed' if item is not added to cart
-        driver.execute_script(
-            'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "iPhone 12 not added to the cart!"}}')
-except NoSuchElementException as err:
-    message = 'Exception: ' + str(err.__class__) + str(err.msg)
-    driver.execute_script(
-        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
-except Exception as err:
-    message = 'Exception: ' + str(err.__class__) + str(err.msg)
-    driver.execute_script(
-        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": ' + json.dumps(message) + '}}')
-finally:
-    # Stop the driver
-    driver.quit()
+driver.quit()
