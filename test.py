1429e9042f4b3987f7fb25538da28b6be68d0ecf
KhushbuRander <123160999+KhushbuRander@users.noreply.github.com>
2023-01-21 22:18:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 userName = 'khushburander_ca4jQ5'
 accessKey = 'AnzFx7qCZUyqqSEe42ym'
