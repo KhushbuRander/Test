b23d139d14c5fe673d3b6dcc61935dcc8211b470
KhushbuRander <123160999+KhushbuRander@users.noreply.github.com>
2023-01-21 11:10:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ from selenium.common.exceptions import NoSuchElementException
 # without any changes to the test files!
 #driver = webdriver.Chrome()
 
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
