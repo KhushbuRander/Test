2dadec1e79771026e8bffcbb25317d4516934c9e
KhushbuRander <123160999+KhushbuRander@users.noreply.github.com>
2023-01-21 22:29:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+import os
 
 userName = os.environ['BROWSERSTACK_USERNAME']
 accessKey = os.environ['BROWSERSTACK_ACCESS_KEY']
