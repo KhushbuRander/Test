789495c586dfac8e4d18bf1e4c39d06af49aa596
KhushbuRander <123160999+KhushbuRander@users.noreply.github.com>
2023-01-27 13:51:19 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -12,6 +12,7 @@ from selenium.common.exceptions import NoSuchElementException
 #from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
 
 userName = os.environ['BROWSERSTACK_USERNAME']
 accessKey = os.environ['BROWSERSTACK_ACCESS_KEY']
